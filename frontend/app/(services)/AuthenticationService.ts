7c0fb99de12e2783b36bfc35c49f27fb4e32aeba
davidemarcoli <davide@marcoli.ch>
2024-01-15 19:52:08 +0100

    Change username to email in auth

--- frontend/app/(services)/AuthenticationService.ts
+++ frontend/app/(services)/AuthenticationService.ts
@@ -6,23 +6,23 @@ import ApiService from './ApiService'
 const baseURL = "/auth/"
 
 const AuthenticationService = () => ({
-  login: async (username:string, password: string) => {
-      const params = new URLSearchParams();
-      params.append("username", username);
-      params.append("password", password);
-      const {data} = await ApiService.post(baseURL + "login", params);
-      return data;
-  },
-  requestNewToken: async () => {
-      const {data} = await ApiService.get(baseURL + "refresh-token", {
-          headers: {Authorization: "Bearer " + CookieUtility.get(REFRESH_TOKEN)},
-      })
-      return data;
-    },
-  signup:async (user:SignUpUserDTO) => {
+login: async (email: string, password: string) => {
+    // const params = new URLSearchParams();
+    // params.append("username", username);
+    // params.append("password", password);
+    const {data} = await ApiService.post(baseURL + "login", {email, password});
+    return data;
+},
+requestNewToken: async () => {
+    const {data} = await ApiService.get(baseURL + "refresh-token", {
+        headers: {Authorization: "Bearer " + CookieUtility.get(REFRESH_TOKEN)},
+    })
+    return data;
+},
+signup: async (user: SignUpUserDTO) => {
     console.log({...user})
     return ApiService.post(baseURL + "signup", {...user})
-  }
+}
 })
 
 export default AuthenticationService
