74ec008e70ad1ad99ebdcb3e65a39093d9127ee1
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2024-01-16 14:57:42 +0100

    changed loadprofile

--- backend/src/main/kotlin/com/litter/dating/litterchatbackend/controller/UserController.kt
+++ backend/src/main/kotlin/com/litter/dating/litterchatbackend/controller/UserController.kt
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.*
 @CrossOrigin
 @RequestMapping("/user")
 class UserController(private val userRepository: UserRepository) {
-    @GetMapping("profile/{username}")
-    fun getProfile(@PathVariable username: String): ResponseEntity<Profile> {
-        val user = userRepository.findByUsername(username)
-            ?: throw Exception("User not found")
+    @GetMapping("/profile/{id}")
+    fun getProfile(@PathVariable id: String): ResponseEntity<Profile> {
+        val user = userRepository.findById(id).orElseThrow()
         val profile = user.profile
             ?: throw Exception("Profile not found")
         return ResponseEntity(profile, HttpStatus.OK)
