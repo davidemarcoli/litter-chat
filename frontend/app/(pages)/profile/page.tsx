2f779d9c50cf5734aa237319e50901abf0263610
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2024-01-23 16:53:00 +0100

    minor update to profile page.

--- frontend/app/(pages)/profile/page.tsx
+++ frontend/app/(pages)/profile/page.tsx
@@ -43,7 +43,7 @@ const ProfilePage = () => {
   return (
     <div className="flex items-center justify-center overflow-hidden mx-[10%] my-[2%] rounded-3xl" style={{height: "90vh"}}>
 
-        <div className="w-1/3 h-[100%] items-center pl-4">
+        <div className="w-1/3 h-[100%] items-center pl-4 bg-slate-400">
             <Link className="my-5 flex flex-row" href="/chat">
               <ArrowLeftFromLine/>
               <p className='text-lg ml-4'>Go Back</p>
@@ -53,9 +53,9 @@ const ProfilePage = () => {
 
             {tabs.map((item, index) => (
               <ul key={index}>
-                <div className='bg-orange-400' onClick={() => traverseTab(item)}>
-                  <p className='text-xl my-5'>{item}</p>
-                  <Divider className="my-5 w-2/3" />
+                <div className='mx-[5%]' onClick={() => traverseTab(item)}>
+                  <p className='text-xl my-5 cursor-pointer'>{item}</p>
+                  <Divider className="my-5" />
                 </div>
               </ul>
             ))}
--- frontend/components/profile/PreferencesOverview.tsx
+++ frontend/components/profile/PreferencesOverview.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from '@/components/theme/mode-toggle'
 const PreferencesOverview = () => {
   return (
     <div className="">
-      <h1 className="text-lg font-medium">Profile</h1>
+      <h1 className="text-lg font-medium">Prefernces</h1>
       <p className="text-sm text-muted-foreground">
         This is how others will see you on the site.
       </p>
